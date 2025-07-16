c2a6bc40cc661930413b2cd1db899e941bd41184
Hen Heang <henheang15@gmail.com>
2025-07-16 10:40:44 +0700

    add todo list

---
+++ todoapi/src/main/java/com/test/todoapi/controller/TodoController.java
@@ -0,0 +1,17 @@
+package com.test.todoapi.controller;
+
+import com.henheang.authhub.controller.BaseController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/todo/v1")
+@RequiredArgsConstructor
+
+
+public class TodoController extends BaseController {
+
+
+}
