7c1911840c8726335d3747eec19a47b6e68171ec
Hen-Heang <heangofficial168@gmail.com>
2025-07-29 23:47:48 +0700

    fix bug

--- todoapi/src/main/java/com/test/todoapi/payload/TodoCommentResponse.java
+++ todoapi/src/main/java/com/test/todoapi/payload/TodoCommentResponse.java
@@ -1,16 +1,12 @@
 package com.test.todoapi.payload;
 
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 
 public class TodoCommentResponse {
 
